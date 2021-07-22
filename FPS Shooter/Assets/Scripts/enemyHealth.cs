d15d1df0a02f4e8b9836983e314d0dee512af4d2
xxudon <ahlinder.leo@icloud.com>
2021-07-22 21:15:35 +0200

    Player able to shoot and kill enemies

---
+++ FPS Shooter/Assets/Scripts/enemyHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyHealth : MonoBehaviour
+{
+    public float health = 3f;
+    // Update is called once per frame
+
+    public void Damage(float damageAmount)
+    {
+        health -= damageAmount;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+ 
+}
+
---
+++ FPS Shooter/Assets/Scripts/playerAction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerAction : MonoBehaviour
+{
+    enemyHealth enemiesHealth;
+    private float gunEquipped = 0f;//varje siffra är för olika vapen 0 är för inget vapen.
+    public float gunDamage = 1f;
+    private void Awake()
+    {
+        enemiesHealth = GameObject.Find("TestEnemy").GetComponent<enemyHealth>();
+    }
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray,out hit))
+        {
+            if (hit.collider.CompareTag("Enemy") && Input.GetMouseButtonDown(0) && gunEquipped == 0)
+            {
+                Debug.Log("Shooting");
+                enemyHealth health = hit.collider.GetComponent<enemyHealth>();
+                if (health != null)
+                {
+                    health.Damage(gunDamage);
+                }
+            }
+        }
+    }
+}
