92cd70ebbfd20dd0dd9a0eb17128baa94b89d0ba
xxudon <ahlinder.leo@icloud.com>
2021-07-22 20:29:07 +0200

    Simple Player Movement

---
+++ FPS Shooter/Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+    public float Sens = 100f;
+    public Transform playerBody;
+    float xRotation;
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * Sens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * Sens * Time.deltaTime;
+        playerBody.Rotate(Vector3.up * mouseX);
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation -= mouseY;
+    }
+}
---
+++ FPS Shooter/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController CC;
+    public float speed = 4f;
+    public LayerMask groundMask;
+    float gravity = -10f;
+    Vector2 velocity;
+    public Transform groundCheck;
+    bool isGrounded;
+    float jumpHeight = 2f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.4f, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        CC.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        CC.Move(velocity * Time.deltaTime);
+
+    }
+}
